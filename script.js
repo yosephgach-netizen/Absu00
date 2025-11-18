2715fe339dee242b849088fc3a1f92cc1358fc39
Absu <yosephgach@gmail.com>
2025-11-18 19:11:37 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMenu() {
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.nav-menu');
+    
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+}
+
+function closeMenu() {
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.nav-menu');
+    
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+}
